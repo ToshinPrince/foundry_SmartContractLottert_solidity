727f8196d4ca2fc5e4a79b2b3084974fa6911265
Toshin Prince <toshinprince@gmail.com>
2024-06-06 12:51:43 +0530

    HelperConfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -14,7 +15,17 @@ contract HelperConfig is Script {
         uint32 callbackGasLimit;
     }
 
-    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+    NetworkConfig public activeNetworkConfig;
+
+    constructor {
+        if(block.chainid == 11155111){
+            activeNetworkConfig = getSepoliaEthConfig();
+        }else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
                 entranceFee: 0.01 ether,
@@ -30,5 +41,26 @@ contract HelperConfig is Script {
         public
         view
         returns (NetworkConfig memory)
-    {}
+    {
+        if(NetworkConfig.vrfCoordinator != address(0)){
+            return activeNetworkConfig;
+        }
+
+        uint96 baseFee = 0.25 ether; //0.25 Link
+        uint96 gasPriceLink = 1e9; // 1 gwei Link
+
+        vm.startBroadcast();
+        VRFCoordinatorV2Mock vrfCoordinatorV2Mock = new VRFCoordinatorV2Mock(baseFee,gasPriceLink);
+        vm.stopBroadcast();
+
+        return NetworkConfig(
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: address(vrfCoordinatorV2Mock),
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 0, //will give later
+                callbackGasLimit: 500000 //500,000)
+        )
+
+    }
 }
