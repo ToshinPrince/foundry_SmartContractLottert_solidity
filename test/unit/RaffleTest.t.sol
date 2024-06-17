0ed9b058fbbd714257c53aa6fe93f7c06f88542d
Toshin Prince <toshinprince@gmail.com>
2024-06-17 14:01:40 +0530

    test: CheckUpKeep

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -80,4 +80,42 @@ contract RaffleTest is Test {
         vm.expectRevert(Raffle.Raffle__RaffleNotOpen.selector);
         raffle.enterRaffle{value: entranceFee}();
     }
+
+    ///////////////////////
+    //CheckUpKeep       //
+    /////////////////////
+
+    function testCheckUpkeepReturnsFalseIfItHasNoBalance() public {
+        //Arrange
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+
+        //Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        //Assert
+        assert(!upkeepNeeded);
+    }
+
+    function testUpkeepReturnsFalseIfRaffleNotOpen() public {
+        //Arrange
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        raffle.performUpkeep("");
+
+        //Act
+        (bool upkeepNeeded, ) = raffle.checkUpkeep("");
+
+        //Assert
+        assert(upkeepNeeded == false);
+    }
+
+    //testUpkeepReturnsFalseIfEnoughTimeHasntPassed
+    //testUpkeepReturnsTrueWhenParametersAreGood
+
+    //////////////////////
+    // performUpkeep  ///
+    ////////////////////
 }
